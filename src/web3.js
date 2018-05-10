4a72023b3680f3895f276db890acbbc56f1820da
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2018-05-10 14:47:19 -0300

    Load node if not client (kovan for now)

--- src/web3.js
+++ src/web3.js
@@ -1,12 +1,15 @@
 import Web3 from 'web3';
+
 const web3 = new Web3();
+
 export default web3;
 
+const settings = require('./settings');
 const initWeb3 = () => {
   if (window.web3) {
     web3.setProvider(window.web3.currentProvider);
   } else {
-    web3.setProvider('');
+    web3.setProvider(new Web3.providers.HttpProvider(settings.nodeURL));
   }
   window.web3 = web3;
 
