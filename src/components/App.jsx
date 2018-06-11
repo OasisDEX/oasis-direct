9788d3517a73b47391cacf11afa77df995cb66e2
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2018-06-11 17:55:09 -0300

    Widget for iframe

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -151,6 +151,7 @@ class App extends Component {
   }
 
   componentDidMount = () => {
+    this.setHashSection();
     setTimeout(this.listenOnHashChange, 500);
   }
 
@@ -1233,103 +1234,113 @@ class App extends Component {
   }
   //
 
+  renderWidget = () => {
+    return <Widget isConnected={this.state.network.isConnected}
+            section={this.state.section}
+            network={this.state.network.network}
+            loadingAddress={this.state.network.loadingAddress}
+            account={this.state.network.defaultAccount}
+            proxy={this.state.proxy}
+            trade={this.state.trade}
+            balances={this.state.balances}
+            showTxMessage={this.state.showTxMessage}
+            transactions={this.state.transactions}
+            setMainState={this.setMainState}
+            fasterGasPrice={this.fasterGasPrice}
+            doTrade={this.doTrade}
+            reset={this.reset}
+            getProxy={this.getProxy}
+            calculateBuyAmount={this.calculateBuyAmount}
+            calculatePayAmount={this.calculatePayAmount}
+            cleanInputs={this.cleanInputs}
+            setWeb3WebClient={this.setWeb3WebClient}
+            hw={this.state.hw}
+            showHW={this.showHW}
+            showClientChoice={this.showClientChoice}
+            loadHWAddresses={this.loadHWAddresses}
+            selectHWAddress={this.selectHWAddress}
+            importAddress={this.importAddress}/>
+  }
+
   render = () => {
     return (
-      <section>
-        <section>
-          <header className="Container">
-            <div className={`Logo Logo--no-margin`}>
-              <a href="/"> <Logo/> </a>
-            </div>
-            <div className={'NavigationLinks'}>
-              <a href="/#" style={{color: 'white'}}>Trade</a>
-              <a href="/#tax-exporter" style={{color: 'white'}}>Export Trades</a>
-            </div>
-          </header>
-        </section>
-        {
-          this.state.section === 'faq'
-            ? <FAQ/>
-            : <section className="Content">
-              <main className="Container">
-                <div>
-                  <div className="MainHeading">
-                    <h1>THE FIRST DECENTRALIZED INSTANT MARKETPLACE</h1>
+      this.state.section === 'trade-widget'
+        ?
+        this.renderWidget()
+        :
+        <section className="bg-section">
+          <section>
+            <header className="Container">
+              <div className={`Logo Logo--no-margin`}>
+                <a href="/"> <Logo/> </a>
+              </div>
+              <div className={'NavigationLinks'}>
+                <a href="/#" style={{color: 'white'}}>Trade</a>
+                <a href="/#tax-exporter" style={{color: 'white'}}>Export Trades</a>
+              </div>
+            </header>
+          </section>
+          {
+            this.state.section === 'faq'
+              ?
+              <FAQ/>
+              :
+              <section className="Content">
+                <main className="Container">
+                  <div>
+                    <div className="MainHeading">
+                      <h1>THE FIRST DECENTRALIZED INSTANT MARKETPLACE</h1>
+                    </div>
+                    <div className="SecondaryHeading">
+                      <h2>No Registration. No Fees.</h2>
+                    </div>
                   </div>
-                  <div className="SecondaryHeading">
-                    <h2>No Registration. No Fees.</h2>
-                  </div>
-                </div>
-                <Widget isConnected={this.state.network.isConnected}
-                        section={this.state.section}
-                        network={this.state.network.network}
-                        loadingAddress={this.state.network.loadingAddress}
-                        account={this.state.network.defaultAccount}
-                        proxy={this.state.proxy}
-                        trade={this.state.trade}
-                        balances={this.state.balances}
-                        showTxMessage={this.state.showTxMessage}
-                        transactions={this.state.transactions}
-                        setMainState={this.setMainState}
-                        fasterGasPrice={this.fasterGasPrice}
-                        doTrade={this.doTrade}
-                        reset={this.reset}
-                        getProxy={this.getProxy}
-                        calculateBuyAmount={this.calculateBuyAmount}
-                        calculatePayAmount={this.calculatePayAmount}
-                        cleanInputs={this.cleanInputs}
-                        setWeb3WebClient={this.setWeb3WebClient}
-                        hw={this.state.hw}
-                        showHW={this.showHW}
-                        showClientChoice={this.showClientChoice}
-                        loadHWAddresses={this.loadHWAddresses}
-                        selectHWAddress={this.selectHWAddress}
-                        importAddress={this.importAddress}/>
-              </main>
-            </section>
-        }
-        <section>
-          <footer className="Container">
-            <div className="LinksWrapper">
-              <h1> Resources </h1>
-              <ul className="Links">
-                <li className="Link"><a href="https://developer.makerdao.com/" target="_blank"
-                                        rel="noopener noreferrer">Documentation</a></li>
-                <li className="Link"><a href="OasisToS.pdf" target="_blank" rel="noopener noreferrer">Legal</a></li>
-                <li className="Link" onClick={() => {
-                  window.scrollTo(0, 0);
-                }}><a href="/#faq" style={{color: 'white'}}>FAQ</a></li>
-              </ul>
-            </div>
-            <div className="LinksWrapper">
-              <h1> Oasis </h1>
-              <ul className="Links">
-                <li className="Link"><a href="https://oasisdex.com" target="_blank" rel="noopener noreferrer">Oasisdex.com</a>
-                </li>
-                {/* <li className="Link"><a href="#a" target="_blank" rel="noopener noreferrer">Oasis.tax</a></li> */}
-              </ul>
-            </div>
-            <div className="LinksWrapper">
-              <h1> Maker </h1>
-              <ul className="Links">
-                <li className="Link"><a href="https://chat.makerdao.com" target="_blank"
-                                        rel="noopener noreferrer">Chat</a></li>
-                <li className="Link"><a href="https://www.reddit.com/r/MakerDAO/" target="_blank"
-                                        rel="noopener noreferrer">Reddit</a></li>
-              </ul>
-            </div>
-            <div className="LinksWrapper">
-              <h1> Follow us </h1>
-              <ul className="Links">
-                <li className="Link"><a href="https://twitter.com/oasisdirect" target="_blank"
-                                        rel="noopener noreferrer">Twitter</a></li>
-                <li className="Link"><a href="https://steemit.com/@oasisdirect" target="_blank"
-                                        rel="noopener noreferrer">Steem</a></li>
-              </ul>
-            </div>
-          </footer>
+                  {this.renderWidget()}
+                </main>
+              </section>
+          }
+          <section>
+            <footer className="Container">
+              <div className="LinksWrapper">
+                <h1> Resources </h1>
+                <ul className="Links">
+                  <li className="Link"><a href="https://developer.makerdao.com/" target="_blank"
+                                          rel="noopener noreferrer">Documentation</a></li>
+                  <li className="Link"><a href="OasisToS.pdf" target="_blank" rel="noopener noreferrer">Legal</a></li>
+                  <li className="Link" onClick={() => {
+                    window.scrollTo(0, 0);
+                  }}><a href="/#faq" style={{color: 'white'}}>FAQ</a></li>
+                </ul>
+              </div>
+              <div className="LinksWrapper">
+                <h1> Oasis </h1>
+                <ul className="Links">
+                  <li className="Link"><a href="https://oasisdex.com" target="_blank" rel="noopener noreferrer">Oasisdex.com</a>
+                  </li>
+                  {/* <li className="Link"><a href="#a" target="_blank" rel="noopener noreferrer">Oasis.tax</a></li> */}
+                </ul>
+              </div>
+              <div className="LinksWrapper">
+                <h1> Maker </h1>
+                <ul className="Links">
+                  <li className="Link"><a href="https://chat.makerdao.com" target="_blank"
+                                          rel="noopener noreferrer">Chat</a></li>
+                  <li className="Link"><a href="https://www.reddit.com/r/MakerDAO/" target="_blank"
+                                          rel="noopener noreferrer">Reddit</a></li>
+                </ul>
+              </div>
+              <div className="LinksWrapper">
+                <h1> Follow us </h1>
+                <ul className="Links">
+                  <li className="Link"><a href="https://twitter.com/oasisdirect" target="_blank"
+                                          rel="noopener noreferrer">Twitter</a></li>
+                  <li className="Link"><a href="https://steemit.com/@oasisdirect" target="_blank"
+                                          rel="noopener noreferrer">Steem</a></li>
+                </ul>
+              </div>
+            </footer>
+          </section>
         </section>
-      </section>
     );
   }
 }
