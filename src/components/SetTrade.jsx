f71233bb35b121482b18f1a920dffdf76e683710
Pex <georgi.h.georgieff@gmail.com>
2018-06-25 11:58:48 +0200

    Remove redundant code

--- src/components/SetTrade.jsx
+++ src/components/SetTrade.jsx
@@ -12,18 +12,6 @@ import * as Blockchain from "../blockchainHandler";
 
 const settings = require('../settings');
 
-const backdropStyle = {
-  position: 'absolute',
-  display:"flex",
-  alignItems:"center",
-  justifyContent:"center",
-  top: 0,
-  left: 0,
-  backgroundColor: 'rgba(0, 0, 0, 0.5)',
-  height: '100%',
-  width: '100%'
-};
-
 //TODO: make this bound to the token selector.
 const tokens = {
   eth: {
