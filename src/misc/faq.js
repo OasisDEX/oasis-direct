f8c73b88390b772466520614a2eda1b52d6377fd
Pex <georgi.h.georgieff@gmail.com>
2018-06-26 13:57:28 +0200

    Revert "Update FAQ for 1.5 release"
    This reverts commit a4797665a601ac02d7adadb7c1f74e3c09a17802.

--- src/misc/faq.js
+++ src/misc/faq.js
@@ -6,36 +6,28 @@ const FAQ = [
     answer: "Oasis.Direct is the most convenient, fully-decentralized way to change tokens. You don’t need to think about wrapping/unwrapping Ether or any other Ethereum token, you don’t need to worry about the current liquidity and you are always guaranteed the best available price on Oasis.DEX. Simply specify the amount that you want to trade and confirm the transaction."
   },
   {
-    question: "How do you determine the trade price?",
-    answer: "The trade price based on the order volume and current order book depth at OasisDex. For example, assume there are three standing orders on the ETH/DAI order book, each with sell offers of 10 ETH for 5000, 5100 and 5200 DAI respectively. If a user wanted to buy 5 ETH, the estimated trade price will be 500 ETH/DAI (half of the first sell order will be filled). However if the user wanted to buy 20 ETH, the estimated trade price will be 505 ETH/DAI (two sell orders will be filled so ultimately 20 ETH will be bought for 1010 DAI)." 
-  },
-  {
-    question: "What is Price Impact?",
-    answer: "Price impact is an indicator how the estimated trade price differs from the current best order on the order book. If user’s order can be entirely filled by the best order, price impact will be 0%."
-  },
-  {
-    question: "What is Slippage Limit? ",
-    anwer: "The slippage limit value is used to create a Limit Order for your transaction. It ensures that the actual trade price does not differ substantially from the estimated trade price that was accepted by the user. If the order book changed significantly between estimating the price and the trade execution the order will not be filled. With Slippage Limit 2% OasisDirect will execute fill-or-kill Limit Order with the price limit set 2% above the accepted estimated trade price."
-  },
-  {
-    question: "What happens if the order fails? Will I still have to pay gas?",
-    answer: "Yes, you will still pay for gas. Bear in mind that the likelihood that your order can’t be filled is quite low, and will only occur if the price has moved in excess of the slippage limit in the time between you receiving the quote and your transaction getting mined."
-  },
-  {
-    question: "Why is the Slippage Limit different for some pairs ?",
-    answer: "The slippage limits were decided by analyzing the volatility and book depth historically on the Oasis.Dex for the given pairs. The limit might be adjusted accordingly if the market on Oasis.DEX changes. In the future, you will be able to set the slippage limit manually according to your personal preference."
-  },
-  {
-    question: "Can you list the Slippage Limit for all assets/tokens ?",
-    answer: "Right now the slippage limit is 1% for MKR/ETH and MKR/DAI pairs, and 2% for ETH/DAI pair."
-  },
-  {
     question: "Are you going to support more trading pairs in the future ?",
     answer: "Yes, more trading pairs will be supported in the future. In the beginning the following pairs are available: DAI/ETH, MKR/DAI and ETH/MKR."
   },
   {
-    question: "Why do I sometimes need to confirm three transactions, sometimes two transactions, and sometimes only one? ",
-    answer: "The transaction process is simplified by bundling multiple transactions into a single call through the use of a special smart contract called Proxy Contract. This makes it much easier to do multi-step trading operations on OasisDirect. If user does not have a Proxy, an additional transaction is required to create one. Additionally the Proxy has to be given an approval for trading a specific ERC-20 token. If a user already has a Proxy with appropriate approvals, only one transaction per trade will be needed."
+    question: "Why do I sometimes need to confirm two transactions, and sometimes only one ?",
+    answer: "Each trading pair requires a one-time transaction per Ether address to be enabled for trading. Once that is done, all subsequent trades will require confirming just one Ethereum transaction. You just need to confirm two Ethereum transactions if it is the first time you are trading a particular pair from the new Ethereum address."
+  },
+  {
+    question: "What does “Order estimation may vary” mean ?",
+    answer: "The slippage limit value is used to create a Limit Order for your transaction. If this Limit Order cannot be immediately completely filled on Oasis.DEX, the transaction will fail. It’s impossible to guarantee that your order will succeed, since it is being matched with other orders on Oasis.DEX in real time. The fact that Oasis.Direct uses Limit Orders, rather than Market Orders, ensures that a user won’t experience unexpected slippage."
+  },
+  {
+    question: "What happens if the transaction price exceeds the given threshold ? Will I still pay gas price for the transaction ?",
+    answer: "Yes, you will still pay for gas. Bear in mind that the likelihood that your order can’t be filled is quite low, and will only occur if the price has moved in excess of the slippage limit in the time between you receiving the quote and your transaction getting mined."
+  },
+  {
+    question: "Why is the “order estimation vary indicator” (threshold) different for some pairs ?",
+    answer: "The slippage limits were decided by analyzing the volatility and book depth historically on the Oasis.Dex for the given pairs. The limit might be adjusted accordingly if the market on Oasis.DEX changes. In the future, you will be able to set the slippage limit manually according to your personal preference."
+  },
+  {
+    question: "Can you list order estimation variations (thresholds) for all assets/tokens ?",
+    answer: "Right now there is a fixed threshold of 1% for ETH/MKR and DAI/MKR pairs, and 2% for DAI/ETH pair."
   },
   {
     question: "What is the maximum value I can trade through Oasis.Direct ?",
