c4dcf938d42335c97085204243f22371dfa59697
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2018-07-17 14:56:43 -0300

    Move stores and providers to App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
-import {observer} from "mobx-react";
+import {observer, Provider} from "mobx-react";
 
 import FAQ from "./FAQ";
 import Widget from "./Widget";
+
+import network from "../stores/Network";
+import profile from "../stores/Profile";
+import transactions from "../stores/Transactions";
+import system from "../stores/System";
 
 import * as Blockchain from "../blockchainHandler";
 import { Logo } from "./Icons";
 
 window.Blockchain = Blockchain;
+window.network = network;
+window.profile = profile;
+window.transactions = transactions;
+window.system = system;
 
 class App extends Component {
   constructor() {
@@ -36,73 +45,77 @@ class App extends Component {
 
   render = () => {
     return (
-      this.state.section === "trade-widget"
-      ?
-        <Widget section={this.state.section} />
-      :
-        <section className="bg-section">
-          <section>
-            <header className="Container">
-              <div className="Logo Logo--no-margin">
-                <a href="/"><Logo/></a>
-              </div>
-            </header>
-          </section>
-          {
-            this.state.section === "faq"
-            ?
-              <FAQ/>
-            :
-              <section className="Content">
-                <main className="Container">
-                  <div>
-                    <div className="MainHeading">
-                      <h1>THE FIRST DECENTRALIZED INSTANT MARKETPLACE</h1>
-                    </div>
-                    <div className="SecondaryHeading">
-                      <h2>No Registration. No Fees.</h2>
-                    </div>
+      <Provider network={network} profile={profile} transactions={transactions} system={system}>
+        {
+          this.state.section === "trade-widget"
+          ?
+            <Widget section={this.state.section} />
+          :
+            <section className="bg-section">
+              <section>
+                <header className="Container">
+                  <div className="Logo Logo--no-margin">
+                    <a href="/"><Logo/></a>
                   </div>
-                  <Widget section={this.state.section} />
-                </main>
+                </header>
               </section>
-          }
-          <section>
-            <footer className="Container">
-              <div className="LinksWrapper">
-                <h1>Resources</h1>
-                <ul className="Links">
-                  <li className="Link"><a href="https://developer.makerdao.com/" target="_blank" rel="noopener noreferrer">Documentation</a></li>
-                  <li className="Link"><a href="OasisToS.pdf" target="_blank" rel="noopener noreferrer">Legal</a></li>
-                  <li className="Link" onClick={() => {
-                    window.scrollTo(0, 0);
-                  }}><a href="/#faq" style={{color: 'white'}}>FAQ</a></li>
-                </ul>
-              </div>
-              <div className="LinksWrapper">
-                <h1>Oasis</h1>
-                <ul className="Links">
-                  <li className="Link"><a href="https://oasisdex.com" target="_blank" rel="noopener noreferrer">Oasisdex.com</a>
-                  </li>
-                </ul>
-              </div>
-              <div className="LinksWrapper">
-                <h1>Maker</h1>
-                <ul className="Links">
-                  <li className="Link"><a href="https://chat.makerdao.com" target="_blank" rel="noopener noreferrer">Chat</a></li>
-                  <li className="Link"><a href="https://www.reddit.com/r/MakerDAO/" target="_blank" rel="noopener noreferrer">Reddit</a></li>
-                </ul>
-              </div>
-              <div className="LinksWrapper">
-                <h1>Follow us</h1>
-                <ul className="Links">
-                  <li className="Link"><a href="https://twitter.com/oasisdirect" target="_blank" rel="noopener noreferrer">Twitter</a></li>
-                  <li className="Link"><a href="https://steemit.com/@oasisdirect" target="_blank" rel="noopener noreferrer">Steem</a></li>
-                </ul>
-              </div>
-            </footer>
-          </section>
-        </section>
+              {
+                this.state.section === "faq"
+                ?
+                  <FAQ/>
+                :
+                  <section className="Content">
+                    <main className="Container">
+                      <div>
+                        <div className="MainHeading">
+                          <h1>THE FIRST DECENTRALIZED INSTANT MARKETPLACE</h1>
+                        </div>
+                        <div className="SecondaryHeading">
+                          <h2>No Registration. No Fees.</h2>
+                        </div>
+                      </div>
+                      <Widget section={this.state.section} />
+                    </main>
+                  </section>
+              }
+              <section>
+                <footer className="Container">
+                  <div className="LinksWrapper">
+                    <h1>Resources</h1>
+                    <ul className="Links">
+                      <li className="Link"><a href="https://developer.makerdao.com/" target="_blank" rel="noopener noreferrer">Documentation</a></li>
+                      <li className="Link"><a href="OasisToS.pdf" target="_blank" rel="noopener noreferrer">Legal</a></li>
+                      <li className="Link" onClick={() => {
+                        window.scrollTo(0, 0);
+                      }}><a href="/#faq" style={{color: 'white'}}>FAQ</a></li>
+                    </ul>
+                  </div>
+                  <div className="LinksWrapper">
+                    <h1>Oasis</h1>
+                    <ul className="Links">
+                      <li className="Link"><a href="https://oasisdex.com" target="_blank" rel="noopener noreferrer">Oasisdex.com</a>
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="LinksWrapper">
+                    <h1>Maker</h1>
+                    <ul className="Links">
+                      <li className="Link"><a href="https://chat.makerdao.com" target="_blank" rel="noopener noreferrer">Chat</a></li>
+                      <li className="Link"><a href="https://www.reddit.com/r/MakerDAO/" target="_blank" rel="noopener noreferrer">Reddit</a></li>
+                    </ul>
+                  </div>
+                  <div className="LinksWrapper">
+                    <h1>Follow us</h1>
+                    <ul className="Links">
+                      <li className="Link"><a href="https://twitter.com/oasisdirect" target="_blank" rel="noopener noreferrer">Twitter</a></li>
+                      <li className="Link"><a href="https://steemit.com/@oasisdirect" target="_blank" rel="noopener noreferrer">Steem</a></li>
+                    </ul>
+                  </div>
+                </footer>
+              </section>
+            </section>
+        }
+      </Provider>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,25 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {Provider} from "mobx-react";
 
 import App from "./components/App";
 
-import network from "./stores/Network";
-import profile from "./stores/Profile";
-import transactions from "./stores/Transactions";
-import system from "./stores/System";
-
 import "./index.css";
-
-window.network = network;
-window.profile = profile;
-window.transactions = transactions;
-window.system = system;
 
 window.addEventListener("load", () => {
   ReactDOM.render((
-    <Provider network={network} profile={profile} transactions={transactions} system={system}>
       <App />
-    </Provider>
   ), document.getElementById("root"));
 });
