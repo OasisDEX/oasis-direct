8f3b1c455073e28acb2c225c79e561b127312803
Pex <georgi.h.georgieff@gmail.com>
2018-10-19 13:31:43 +0200

    Some refactoring on the test

--- cypress/integration/Trades.spec.js
+++ cypress/integration/Trades.spec.js
@@ -11,36 +11,35 @@ context('Trading', () => {
   it("should create a proxy and sell ETH for DAI", () => {
     cy.get(tid("wallets-continue")).contains("Continue").click();
 
-    cy.get(tid("set-trade-from-amount")).within(() => {
-      cy.get('input').type(ETH_AMOUNT_TO_SELL);
-    });
+    cy.get(tid("set-trade-from-amount"))
+      .find('input').type(ETH_AMOUNT_TO_SELL);
 
     cy.wait(1000);
 
-    cy.get(tid("set-trade-to-amount")).within(() => {
-      cy.get('input').should('have.value', `${DAI_AMOUNT_TO_RECEIVE}.00000`);
-    });
+    cy.get(tid("set-trade-to-amount"))
+      .find('input').should('have.value', `${DAI_AMOUNT_TO_RECEIVE}.00000`);
+
 
     cy.get(tid("terms-and-conditions")).click();
     cy.get(tid("initiate-trade")).click();
 
-    cy.get(tid("trade-with-builtin-proxy-creation")).within(() => {
-      cy.get('.details').find('.label.vertical-align span').should((value) => {
-        expect(value.text().trim()).to.eq('Create Proxy');
-      });
-    })
-
-    cy.get(tid("trade-token-from")).within(() => {
-      cy.get(tid("token-amount-value")).should((value) => {
-        expect(value.text().trim()).to.eq(`${ETH_AMOUNT_TO_SELL} ETH`);
-      });
+    cy.get(tid("trade-with-builtin-proxy-creation"))
+      .find('.details')
+      .find('.label.vertical-align span').should((value) => {
+      expect(value.text().trim()).to.eq('Create Proxy');
     });
 
-    cy.get(tid("trade-token-to")).within(() => {
-      cy.get(tid("token-amount-value")).should((value) => {
+    cy.get(tid("trade-token-from"))
+      .find(tid("token-amount-value"))
+      .should((value) => {
+        expect(value.text().trim()).to.eq(`${ETH_AMOUNT_TO_SELL} ETH`);
+      });
+
+    cy.get(tid("trade-token-to"))
+      .find(tid("token-amount-value"))
+      .should((value) => {
         expect(value.text().trim()).to.eq(`${DAI_AMOUNT_TO_RECEIVE} DAI`);
       });
-    })
 
     cy.wait(20000);
 
@@ -48,5 +47,4 @@ context('Trading', () => {
       expect(value.text().trim()).to.eq('You have successfully created a Proxy')
     });
   });
-
 });
