f709b765d40c666b6cd75c38ed844fe171f7f414
Pex <georgi.h.georgieff@gmail.com>
2018-10-24 15:31:35 +0200

    [WIP] Selling ETH fro ERC20 token without proxy ( fails ).

--- cypress/integration/Trades.spec.js
+++ cypress/integration/Trades.spec.js
@@ -1,13 +1,18 @@
 import { visitWithWeb3, tid, revertToSnapshot } from "../utils";
 
-context('Trading', () => {
-  const ETH_AMOUNT_TO_SELL = '2';
-  const DAI_AMOUNT_TO_RECEIVE = '555';
+context('Selling', () => {
+  const ETH_AMOUNT_TO_SELL = '1';
+  const DAI_AMOUNT_TO_RECEIVE = '280';
+  const waitForTradeToFinish = 20000;
 
   beforeEach(() => visitWithWeb3());
   afterEach(() => revertToSnapshot());
 
   it("ETH for ERC20 without PROXY", () => {
+    const base = "ETH";
+    const quote = "DAI";
+    const expectedPrice = `277.5 ${base}/${quote}`;
+
     cy.get(tid("wallets-continue")).contains("Continue").click();
 
     cy.get(tid("set-trade-from-amount"))
@@ -56,6 +61,63 @@ context('Trading', () => {
     cy.get(tid("congratulation-message", tid("final-price", tid("token-amount-value"))), {
       timeout: waitForTradeToFinish,
     }).contains(expectedPrice);
+  });
+
+  it("ETH for ERC20 with proxy", () => {
+    cy.get(tid("wallets-continue")).contains("Continue").click();
+
+    cy.get(tid("set-trade-from-amount"))
+      .find('input').type(ETH_AMOUNT_TO_SELL);
+
+    cy.get(tid("terms-and-conditions")).click({position: "topRight", force: true});
+    cy.get(tid("initiate-trade")).click();
+
+    cy.get(tid("new-trade")).click({timeout: waitForTradeToFinish});
+
+    cy.get(tid("set-trade-from-amount"))
+      .find('input').type(ETH_AMOUNT_TO_SELL);
+
+    //  NOTE: we need to click this exact spot to avoid downloading PDF file instead (link is in the button as well)
+    cy.get(tid("terms-and-conditions")).click({position: "topRight", force: true});
+    cy.get(tid("initiate-trade")).click();
+
+    getTradeParameters();
+
+    cy.get(tid("trade-with-builtin-proxy-creation"))
+      .should('not.exist');
+
+    cy.get(tid("trade-token-from"))
+      .find(tid("token-amount-value"))
+      .then((value) => {
+        expect(value.text().trim()).to.eq(`${ETH_AMOUNT_TO_SELL} ETH`);
+      });
+
+    cy.get(tid("trade-token-to"))
+      .find(tid("token-amount-value"))
+      .then((value) => {
+        expect(value.text().trim()).to.eq(`${DAI_AMOUNT_TO_RECEIVE} DAI`);
+      });
+
+    cy.get(tid("proxy-creation-summary"), {timeout: waitForTradeToFinish})
+      .should('not.exist');
+
+    cy.get(tid("congratulation-message"), {timeout: waitForTradeToFinish})
+      .find(tid("sold-token"), tid("token-amount-value"))
+      .then(value =>
+        expect(value.text().trim()).to.eq(`${ETH_AMOUNT_TO_SELL} ${base}`)
+      );
+
+    cy.get(tid("congratulation-message"), {timeout: waitForTradeToFinish})
+      .find(tid("bought-token"), tid("token-amount-value"))
+      .then(value =>
+        expect(value.text().trim()).to.eq(`${DAI_AMOUNT_TO_RECEIVE} ${quote}`)
+      );
+
+    cy.get(tid("congratulation-message"), {timeout: waitForTradeToFinish})
+      .find(tid("final-price"),tid("token-amount-value"))
+      .then(value =>
+        expect(value.text().trim()).to.eq(expectedPrice)
+      );
   });
 
   it("ETH for ERC20 with proxy", () => {
