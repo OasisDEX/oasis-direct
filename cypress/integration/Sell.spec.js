2ef3b251cf276683efdb56effd00c502205c3ce9
Pex <georgi.h.georgieff@gmail.com>
2018-11-20 09:40:18 +0100

    Splitting tests into separate files + some refactoring

--- cypress/integration/Sell.spec.js
+++ cypress/integration/Sell.spec.js
@@ -1,10 +1,8 @@
 import { visitWithWeb3, tid } from '../utils';
 import Trade from '../pages/Trade';
 
-const waitForTradeToFinish = 40000;
-
 const nextTrade = () => {
-  cy.get(tid('new-trade')).click({timeout: waitForTradeToFinish});
+  cy.get(tid('new-trade')).click({timeout: Cypress.env('TRADE_TIMEOUT') });
 };
 
 describe('Selling', () => {
@@ -22,7 +20,7 @@ describe('Selling', () => {
       const price = '280 ETH/DAI';
 
 
-      let trade = new Trade().sell(from)(willPay);
+      const trade = new Trade().sell(from)(willPay);
 
       expect(trade).to.receive(`${willReceive}.00000`);
 
@@ -47,15 +45,15 @@ describe('Selling', () => {
       const willReceive = '280';
       const price = '280 ETH/DAI';
 
-      let trade = new Trade().sell(from)(willPay);
+      const trade = new Trade().sell(from)(willPay);
 
       expect(trade).to.receive(`${willReceive}.00000`);
 
-      let finalization = trade
+      const finalization = trade
         .acceptTerms()
         .execute();
 
-      let summary = finalization
+      const summary = finalization
         .shouldCreateProxy()
         .shouldCommitATrade(willPay, from, willReceive, to);
 
@@ -69,22 +67,22 @@ describe('Selling', () => {
       const willReceiveMore = '275';
       const endPrice = '275 ETH/DAI';
 
-      trade = new Trade().sell(from)(willPay);
+      const secondTrade = new Trade().sell(from)(willPay);
 
-      expect(trade).to.receive(`${willReceiveMore}.00000`);
+      expect(secondTrade).to.receive(`${willReceiveMore}.00000`);
 
-      finalization = trade
+      const anotherFinalizationco = secondTrade
         .acceptTerms()
         .execute();
 
-      summary = finalization
+      const anotherSummary = anotherFinalizationco
         .shouldNotCreateProxy()
         .shouldCommitATrade(willPay, from, willReceiveMore, to);
 
-      summary.expectProxyNotBeingCreated();
-      summary.expectBought(willReceiveMore, to);
-      summary.expectSold(willPay, from);
-      summary.expectPriceOf(endPrice);
+      anotherSummary.expectProxyNotBeingCreated();
+      anotherSummary.expectBought(willReceiveMore, to);
+      anotherSummary.expectSold(willPay, from);
+      anotherSummary.expectPriceOf(endPrice);
     });
   });
 
@@ -332,293 +330,4 @@ describe('Selling', () => {
       summary.expectPriceOf(price);
     });
   });
-});
-
-describe('Buying', () => {
-  beforeEach(() => {
-    visitWithWeb3();
-    cy.get(tid('wallets-continue')).contains('Continue').click();
-  });
-
-  context('ETH for ERC20', () => {
-    it('without proxy', () => {
-      const from = 'ETH';
-      const to = 'DAI';
-      const willPay = '0.35714';
-      const willReceive = '100';
-      const price = '280 ETH/DAI';
-
-
-      let trade = new Trade().buy(to)(willReceive);
-
-      expect(trade).to.pay(`${willPay}`);
-
-      const finalization = trade
-        .acceptTerms()
-        .execute();
-
-      const summary = finalization
-        .shouldCreateProxy()
-        .shouldCommitATrade(willPay, from, willReceive, to);
-
-      summary.expectProxyBeingCreated();
-      summary.expectBought(willReceive, to);
-      summary.expectSold(willPay, from);
-      summary.expectPriceOf(price)
-    });
-
-    it('with proxy', () => {
-      const to = 'DAI';
-      const from = 'ETH';
-      const willReceive = '100';
-
-      const trade = new Trade().buy(to)(willReceive);
-
-      trade
-        .acceptTerms()
-        .execute();
-
-      nextTrade();
-
-      const willReceiveMore = '500';
-      const willPay = '1.80649';
-      const price = '276.77929 ETH/DAI';
-
-      const secondTrade = new Trade().buy(to)(willReceiveMore);
-
-      expect(secondTrade).to.pay(willPay);
-
-      const finalization = trade
-        .acceptTerms()
-        .execute();
-
-      const summary = finalization
-        .shouldNotCreateProxy()
-        .shouldCommitATrade(willPay, from, willReceiveMore, to);
-
-      summary.expectProxyNotBeingCreated();
-      summary.expectBought(willReceiveMore, to);
-      summary.expectSold(willPay, from);
-      summary.expectPriceOf(price)
-    });
-  });
-
-  context('ERC20 for ETH', () => {
-    it('without proxy and allowance', () => {
-      const from = 'DAI';
-      const to = 'ETH';
-      const willPay = '37.25778';
-      const willReceive = '0.12378';
-      const price = '301 ETH/DAI';
-
-      const trade = new Trade().buy(to)(willReceive);
-
-      expect(trade).to.pay(`${willPay}`);
-
-      const finalization = trade
-        .acceptTerms()
-        .execute();
-
-      finalization.shouldCreateProxy();
-      expect(finalization.currentTx).to.succeed();
-
-      finalization.shouldSetAllowanceFor(from);
-      expect(finalization.currentTx).to.succeed();
-
-      const summary = finalization.shouldCommitATrade(willPay, from, willReceive, to);
-
-      summary.expectBought(willReceive, to);
-      summary.expectSold(willPay, from);
-      summary.expectPriceOf(price);
-    });
-
-    it("with proxy and no allowance", () => {
-      const from = 'ETH';
-      const to = 'DAI';
-      const willPay = '1';
-
-      const trade = new Trade()
-        .sell(from)(willPay)
-        .buy(to)()
-        .acceptTerms()
-        .execute();
-
-      nextTrade();
-
-      const nextFrom = 'DAI';
-      const nextTo = 'ETH';
-      const nextWillPay = '37.25778';
-      const nextWillReceive = '0.12378';
-      const price = '301 ETH/DAI';
-
-      const secondTrade = new Trade()
-        .sell(nextFrom)()
-        .buy(nextTo)(nextWillReceive);
-
-      expect(trade).to.receive(`${nextWillReceive}`);
-
-      const nextFinalization = secondTrade
-        .acceptTerms()
-        .execute();
-
-      nextFinalization
-        .shouldNotCreateProxy()
-        .shouldSetAllowanceFor(nextFrom);
-
-      expect(nextFinalization.currentTx).to.succeed();
-
-      const finalSummary = nextFinalization
-        .shouldCommitATrade(nextWillPay, nextFrom, nextWillReceive, nextTo);
-
-      finalSummary.expectProxyNotBeingCreated();
-      finalSummary.expectBought(nextWillReceive, nextTo);
-      finalSummary.expectSold(nextWillPay, nextFrom);
-      finalSummary.expectPriceOf(price);
-    });
-
-    it("with proxy and allowance", () => {
-      const from = 'DAI';
-      const to = 'ETH';
-      const willPay = '37.25778';
-      const willReceive = '0.12378';
-      const price = '301 ETH/DAI';
-
-      new Trade()
-        .sell(from)()
-        .buy(to)(willReceive)
-        .acceptTerms()
-        .execute();
-
-      nextTrade();
-
-      const newTrade = new Trade()
-        .sell(from)()
-        .buy(to)(willReceive);
-
-      const finalization = newTrade
-        .acceptTerms()
-        .execute();
-
-      const summary = finalization
-        .shouldNotCreateProxy()
-        .shouldNotSetAllowance()
-        .shouldCommitATrade(willPay, from, willReceive, to);
-
-      summary.expectBought(willReceive, to);
-      summary.expectSold(willPay, from);
-      summary.expectPriceOf(price);
-    });
-  });
-
-  context('ERC20 for ERC20', () => {
-    it('without proxy and allowance', () => {
-      const from = 'DAI';
-      const to = 'MKR';
-      const willPay = '103.33333';
-      const willReceive = '0.5';
-      const price = '206.66666 MKR/DAI';
-
-      const trade = new Trade()
-        .sell(from)()
-        .buy(to)(willReceive);
-
-      expect(trade).to.pay(`${willPay}`);
-
-      const finalization = trade
-        .acceptTerms()
-        .execute();
-
-      finalization.shouldCreateProxy();
-      expect(finalization.currentTx).to.succeed();
-
-      finalization.shouldSetAllowanceFor(from);
-      expect(finalization.currentTx).to.succeed();
-
-      const summary = finalization.shouldCommitATrade(willPay, from, willReceive, to);
-
-      summary.expectBought(willReceive, to);
-      summary.expectSold(willPay, from);
-      summary.expectPriceOf(price);
-    });
-
-    it("with proxy and no allowance", () => {
-      const from = 'ETH';
-      const to = 'DAI';
-      const willPay = '1';
-
-      const trade = new Trade()
-        .sell(from)(willPay)
-        .buy(to)()
-        .acceptTerms()
-        .execute();
-
-      nextTrade();
-
-      const nextFrom = 'DAI';
-      const nextTo = 'MKR';
-      const nextWillPay = '103.33333';
-      const nextWillReceive = '0.5';
-      const price = '206.66666 MKR/DAI';
-
-      const secondTrade = new Trade()
-        .sell(nextFrom)()
-        .buy(nextTo)(nextWillReceive);
-
-      expect(trade).to.receive(`${nextWillReceive}`);
-
-      const nextFinalization = secondTrade
-        .acceptTerms()
-        .execute();
-
-      nextFinalization
-        .shouldNotCreateProxy()
-        .shouldSetAllowanceFor(nextFrom);
-
-      expect(nextFinalization.currentTx).to.succeed();
-
-      const finalSummary = nextFinalization
-        .shouldCommitATrade(nextWillPay, nextFrom, nextWillReceive, nextTo);
-
-      finalSummary.expectProxyNotBeingCreated();
-      finalSummary.expectBought(nextWillReceive, nextTo);
-      finalSummary.expectSold(nextWillPay, nextFrom);
-      finalSummary.expectPriceOf(price);
-    });
-
-    it("with proxy and allowance", () => {
-      const from = 'DAI';
-      const to = 'MKR';
-      const willPay = '103.33333';
-      const willReceive = '0.5';
-      const price = '206.66666 MKR/DAI';
-
-      new Trade()
-        .sell(from)()
-        .buy(to)(willReceive)
-        .acceptTerms()
-        .execute();
-
-      nextTrade();
-
-      const nextWillReceive = '0.1';
-      const nextWillPay = '20.66666';
-
-      const newTrade = new Trade()
-        .sell(from)()
-        .buy(to)(nextWillReceive);
-
-      const finalization = newTrade
-        .acceptTerms()
-        .execute();
-
-      const summary = finalization
-        .shouldNotCreateProxy()
-        .shouldNotSetAllowance()
-        .shouldCommitATrade(nextWillPay, from, nextWillReceive, to);
-
-      summary.expectBought(nextWillReceive, to);
-      summary.expectSold(nextWillPay, from);
-      summary.expectPriceOf(price);
-    });
-  })
 });
