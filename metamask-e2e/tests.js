76efca02f3a7e380d749d309008ee9facacd14ae
Krzysztof Kaczor <chris@kaczor.io>
2018-11-25 20:42:52 +0100

    first meaningful test with metamask

--- metamask-e2e/tests.js
+++ metamask-e2e/tests.js
@@ -1,19 +1,17 @@
 const Puppeteer = require("puppeteer");
 const { launchPuppeteerWithMetamask, setupMetamask } = require("metamask-puppeteer");
-const { click } = require("puppeteer-utils");
+const { click, waitForText } = require("puppeteer-utils");
 
 const { tid } = require("../cypress/utils/");
 
-const IS_DEV = process.env.NODE_ENV === "dev";
+const IS_DEV = process.env.DEV === "1";
 
 async function main() {
-  // tslint:disable-next-line
   console.log("Starting browser...");
   const browser = await launchPuppeteerWithMetamask(Puppeteer, {
     headless: false,
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
-  // tslint:disable-next-line
   console.log("Browser started");
   const metamaskController = await setupMetamask(browser);
 
@@ -21,18 +19,17 @@ async function main() {
   await oasisPage.goto("http://localhost:3000");
 
   await metamaskController.loadPrivateKey("0x1ff8271bf14ac9bef0b641cced40dc2a7ebd2e37d8e16d25b4aa1911364219af");
-  await metamaskController.changeNetwork("kovan");
+  await metamaskController.changeNetwork("localhost");
 
   await click(oasisPage, tid("wallets-continue"));
 
   await metamaskController.allowToConnect();
-  await oasisPage.bringToFront();
 
-  // tslint:disable-next-line
-  console.assert((await metamaskController.getStatus()) === "unlocked");
+  await waitForText(oasisPage, tid("set-trade-from", tid("token-amount-value")), /8,999.... ETH/);
+  await waitForText(oasisPage, tid("set-trade-to", tid("token-amount-value")), /170 DAI/);
 
-  // tslint:disable-next-line
   console.log("Done!");
+
   // don't close window to early in dev mode
   if (!IS_DEV) {
     await oasisPage.close();
@@ -41,7 +38,6 @@ async function main() {
 }
 
 main().catch(e => {
-  // tslint:disable-next-line
   console.error(e);
   // don't close window to early in dev mode
   if (!IS_DEV) {
