815b3015a8b00e7fd4f72ae397f9629cc9a71697
Krzysztof Kaczor <krzkaczor@gmail.com>
2018-12-07 11:20:44 +0100

    remove wait

--- cypress/integration/Buy.spec.js
+++ cypress/integration/Buy.spec.js
@@ -12,7 +12,6 @@ describe('Buying', () => {
   beforeEach(() => {
     cypressVisitWithWeb3();
     cy.get(tid('wallets-continue')).contains('Continue').click();
-    cy.wait(2000);
   });
 
   context('ETH for ERC20', () => {
