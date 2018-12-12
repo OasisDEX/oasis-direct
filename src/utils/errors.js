d188a3c822e1fc4e470f3542c04284e7888cc4ec
Pex <georgi.h.georgieff@gmail.com>
2018-12-12 21:57:19 +0100

    Display amounts in error messages up to the fifth digit in decimal.

--- src/utils/errors.js
+++ src/utils/errors.js
@@ -1,14 +1,34 @@
 import React from 'react';
-import { formatNumber } from "./helpers";
 
 export const ERRORS = Object.freeze({
   NO_ORDERS: (tradeSide, amount, token) =>
     (
       <React.Fragment key="error">
-        No orders available to {tradeSide} <span className="error-msg value" key="error-amount">{formatNumber(amount,5, false)} {token.toUpperCase()}</span>
+        No orders available to {tradeSide} <span className="error-msg value" key="error-amount">{formatNumber(amount,5)} {token.toUpperCase()}</span>
       </React.Fragment>
     ),
   NO_GAS_FUNDS: `You will not have enough  Ether to pay for the transaction`,
   MINIMAL_VALUE: (threshold, token) => `The Minimum trade value is ${threshold} ${token.toUpperCase()}`,
-  INSUFFICIENT_FUNDS: (amount, token) => `You don't have ${formatNumber(amount,5, false)} ${token.toUpperCase()} in your wallet`
+  INSUFFICIENT_FUNDS: (amount, token) => `You don't have ${formatNumber(amount,5)} ${token.toUpperCase()} in your wallet`
 });
+
+/* This is a TEMPORARY function.
+ * formatNumber#helpers.js must be use.
+ *
+ * The reason why we are not using it is that the latter function
+ * depends on web3 module which depends on ledger-subprovider.js
+ * The syntax includes  the one for Flow typings.
+ *
+ * Out tests fails so this is a way to not block further development.
+* */
+export const formatNumber = (amount, decimals) => {
+  const parts = amount.toString().split(".");
+  const whole = parts[0];
+  const decimal_digits = parts[1];
+
+  if(decimal_digits){
+     return whole + "." + decimal_digits.slice(0,decimals);
+  } else {
+    return amount;
+  }
+};
